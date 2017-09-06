8f5e7c9d77ea3524afcf024f477c0bac7e4ec938
Superball <tangeaim@hotmail.com>
2017-09-06 22:11:03 +0700

    fix: แก้ไขการ logout

--- application/views/template/backheader.php
+++ application/views/template/backheader.php
@@ -113,7 +113,7 @@
 							</a>
 						</div>
 						<div class="pull-right">
-							<a href="<?php echo  base_url('user/logout'); ?>" class="btn btn-default btn-flat">
+							<a href="<?php echo  base_url('dashboard/logout'); ?>" class="btn btn-default btn-flat">
 								Sign out
 							</a>
 						</div>
